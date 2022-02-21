75b08a8476528bc22d3cc8fc9b9ea337bbc9787b
Angel Rodriguez <angelrodri@hotmail.com>
2022-02-21 10:57:37 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("hero").innerHTML = `
+    <div class="character-card">
+        <h4 class="name"> Wizard </h4>
+        <img class="avatar" src="/Module 6/role-playing-game/images/wizard.png"/>
+        <p class="health">health: <b> 60 </b></p>
+        <div class="dice-container"><div class="dice"> 6 </div></div>
+    </div>
+`
+
+document.getElementById("monster").innerHTML = `
+    <div class="character-card">
+        <h4 class="name"> Orc </h4>
+        <img class="avatar" src="/Module 6/role-playing-game/images/orc.png"/>
+        <p class="health">health: <b> 10 </b></p>
+        <div class="dice-container"><div class="dice"> 4 </div></div>
+    </div>
+`
