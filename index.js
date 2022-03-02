62ec0855090b65462789ce27296d183d8167061a
Angel Rodriguez <angelrodri@hotmail.com>
2022-03-02 13:23:04 -0800

    destructed functions for rolling random dice

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function getDiceRollArray(diceCount) {
+    const newDiceRolls = []
+    for (let i = 0; i < diceCount; i++) {
+        newDiceRolls.push(Math.floor(Math.random() * 6) +1)
+    }
+    return newDiceRolls;
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num) {
+        return `
+        <div class="dice">${num}</div>
+        `
+    }).join("")
+}
+
+
 const hero = {
     elementId: "hero",
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: [3, 1, 4],
     diceCount: 3
 }
 
@@ -12,30 +28,21 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: [2],
     diceCount: 1
 }
 
 function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceRoll, diceCount} = data;
+    const {elementId, name, avatar, health, diceCount} = data;
 
-    const diceHtml = diceRoll.map(function(dice) {
-        return `
-        <div class="dice">${dice}</div>
-        `
-    }).join("")
-    
-    // for (let i = 0; i < diceCount; i++) {
-    //     diceHtml += `
-    //     <div class="dice">${diceRoll[i]}</div>
-    //     `
-    // }
+    const diceHtml = getDiceHtml(diceCount)
 
     document.getElementById(elementId).innerHTML = `
     <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health} </b></p>
+        <div class="health">health: 
+            <b> ${health} </b>
+        </div>
         <div class="dice-container">
             ${diceHtml}
         </div>
