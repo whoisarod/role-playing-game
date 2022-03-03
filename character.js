66087c85aa0e71dbe003fa8488f256a5db781367
Angel Rodriguez <angelrodri@hotmail.com>
2022-03-03 15:48:51 -0800

    add files character.js, data.js, and utils js. modified index.js and index.html to import new files

---
+++ character.js
@@ -0,0 +1,31 @@
+import {getDiceRollArray} from "./utils.js";
+
+function Character(data) {
+    Object.assign(this, data)
+
+    this.getCharacterHtml = function(){
+        const {elementId, name, avatar, health, diceCount} = this;
+        let diceHtml = this.getDiceHtml(diceCount)
+
+        return  `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}"/>
+            <div class="health">health: 
+                <b> ${health} </b>
+            </div>
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
+        </div>
+        `
+    }
+
+    this.getDiceHtml = function (diceCount) {
+        return getDiceRollArray(diceCount).map(function(num) {
+            return `<div class="dice">${num}</div>`
+        }).join('')
+    }
+}
+
+export default Character
--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function() {
-        return Math.floor(Math.random() * 6) + 1
-    })
-}
+import characterData from "./data.js"
+import Character from "./character.js"; 
 
-const hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-}
-
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
-}
-
-function Character(data) {
-    Object.assign(this, data)
-
-    this.getCharacterHtml = function(){
-        const {elementId, name, avatar, health, diceCount} = this;
-        let diceHtml = this.getDiceHtml(diceCount)
-
-        return  `
-        <div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}"/>
-            <div class="health">health: 
-                <b> ${health} </b>
-            </div>
-            <div class="dice-container">
-                ${diceHtml}
-            </div>
-        </div>
-        `
-    }
-
-    this.getDiceHtml = function (diceCount) {
-        return getDiceRollArray(diceCount).map(function(num) {
-            return `<div class="dice">${num}</div>`
-        }).join('')
-    }
-} 
-
-const wizard = new Character(hero)
-const orc = new Character(monster)
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 
 function render(){
-    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
-    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()   
+    document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
+    document.getElementById("monster").innerHTML = orc.getCharacterHtml()   
 }
 render()
